40ce9daf276e3de9814eee29030e017148c814d7
tglima <thg.limadesousa@gmail.com>
2021-09-07 19:53:56 -0300

    Adiciona a class ConstantUtil

---
+++ lib/utils/constant_util.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ConstantUtil {
+  //Texts
+  static final String nmApplication = 'MyFlutterCrypto';
+  static final String settings = 'Configurações';
+  static final String about = 'Sobre';
+
+  //Colors
+  static final colorThemeApp = Colors.indigo[500];
+  static final Color colorTxtDefautl = Colors.black87;
+}
