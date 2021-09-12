f6cc5d916c61ad23d56f62e35493a9dac00f543f
tglima <thg.limadesousa@gmail.com>
2021-09-11 22:05:59 -0300

    Refatora a class HomeScreen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:myfluttercrypto/screens/settings_sreen.dart';
 import 'package:myfluttercrypto/utils/constant_util.dart';
+import 'package:myfluttercrypto/utils/wdg_util.dart';
+
+import 'about_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -9,20 +13,30 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-            backgroundColor: ConstantUtil.colorThemeApp,
-            title: Text(ConstantUtil.nmApplication),
-            actions: <Widget>[
-              IconButton(
-                  icon: const Icon(Icons.settings),
-                  tooltip: ConstantUtil.settings,
-                  onPressed: () {}),
-              IconButton(
-                  icon: const Icon(Icons.info),
-                  tooltip: ConstantUtil.about,
-                  onPressed: () {})
-            ]),
-        body: Container());
+    void goToSettings() {
+      Navigator.push(context,
+          MaterialPageRoute(builder: (context) => const SettingsScreen()));
+    }
+
+    void goToAbout() {
+      Navigator.push(context,
+          MaterialPageRoute(builder: (context) => const AboutScreen()));
+    }
+
+    AppBar _buildAppBar = AppBar(
+        backgroundColor: ConstantUtil.colorThemeApp,
+        title: Text(ConstantUtil.nmApplication),
+        actions: <Widget>[
+          IconButton(
+              icon: const Icon(Icons.settings),
+              tooltip: ConstantUtil.settings,
+              onPressed: goToSettings),
+          IconButton(
+              icon: const Icon(Icons.info),
+              tooltip: ConstantUtil.about,
+              onPressed: goToAbout)
+        ]);
+
+    return WdgUtil.buildScaffold(context, _buildAppBar, []);
   }
 }
