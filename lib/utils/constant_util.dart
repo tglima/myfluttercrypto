e3369c87f1e6091e454a10550d27ecb2dae127e6
tglima <thg.limadesousa@gmail.com>
2021-09-11 22:03:34 -0300

    Adiciona novas constantes

--- lib/utils/constant_util.dart
+++ lib/utils/constant_util.dart
@@ -5,8 +5,27 @@ class ConstantUtil {
   static final String nmApplication = 'MyFlutterCrypto';
   static final String settings = 'Configurações';
   static final String about = 'Sobre';
+  static final String copyright = 'Copyright';
+  static final String txtCopyright = ': Copyright © 2021 Thiago Lima de Sousa.';
+  static final String visit = 'Visite';
+  static final String linkGitHub = 'https://github.com/tglima/myfluttercrypto';
+  static final String version = 'Versão';
+  static final String license = 'Licença';
+  static final String twoPoints = ':';
+  static final String txtLicense =
+      ': O código fonte desta aplicação está sob Licença MIT, todo o restante deve ser considerado conteúdo registrado dos seus respectivos proprietários e desenvolvedores.';
 
   //Colors
   static final colorThemeApp = Colors.indigo[500];
-  static final Color colorTxtDefautl = Colors.black87;
+  static final colorTxtUrl = Colors.indigo;
+  static final Color colorTxtDefault = Colors.black87;
+
+  //EdgeInsetsGeometry
+  static final EdgeInsetsGeometry edgeInsetsDefault =
+      EdgeInsets.fromLTRB(16, 8, 16, 8);
+
+  static final EdgeInsetsGeometry edgeInsetsTopZero =
+      EdgeInsets.fromLTRB(16, 0, 16, 8);
+  //Temp
+  static final String nuVersion = ': 0.0.0+1';
 }
