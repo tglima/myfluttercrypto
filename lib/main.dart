0895cb25fd1813585626fef3da272d3f1037f189
tglima <thg.limadesousa@gmail.com>
2021-09-19 14:49:55 -0300

    Atualiza o arquivo main.dart Implementa o método _initPackageInfo

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:myfluttercrypto/screens/home_screen.dart';
 import 'package:myfluttercrypto/utils/constant_util.dart';
+import 'package:package_info_plus/package_info_plus.dart';
+import 'data/global_data.dart';
 
-void main() {
+void main() async {
+  Future<void> _initPackageInfo() async {
+    PackageInfo packageInfo = PackageInfo(
+      appName: '',
+      packageName: '',
+      version: '',
+      buildNumber: '',
+      buildSignature: '',
+    );
+    final info = await PackageInfo.fromPlatform();
+    packageInfo = info;
+    GlobalData.nuVersion = packageInfo.version;
+  }
+
+  await _initPackageInfo();
+
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     home: HomeScreen(),
