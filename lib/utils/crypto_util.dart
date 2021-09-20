26c9467bd1d4681787c04aba8fbd955915997be1
tglima <thg.limadesousa@gmail.com>
2021-09-20 19:07:08 -0300

    Implementa a class CryptoUtil

--- lib/utils/crypto_util.dart
+++ lib/utils/crypto_util.dart
@@ -1,11 +1,30 @@
+import 'package:myfluttercrypto/data/global_data.dart';
 import 'package:myfluttercrypto/data/result_data.dart';
+import 'package:encrypt/encrypt.dart';
 
 class CryptoUtil {
-  ResultData txtDecrypt(String txt) {
-    return ResultData(false, txt: txt);
+  final iv = IV.fromLength(16);
+  final String endKey = '__JesusS4lv@';
+  dynamic key, encrypter;
+
+  CryptoUtil() {
+    key = Key.fromUtf8(GlobalData.keyWord + endKey);
+    encrypter = Encrypter(AES(key, mode: AESMode.ofb64Gctr));
   }
 
   String txtEncrypt(String txt) {
-    return txt;
+    return encrypter.encrypt(txt, iv: iv).base64;
+  }
+
+  ResultData txtDecrypt(String txt) {
+    ResultData result;
+    try {
+      result = ResultData(false,
+          txt: encrypter.decrypt(Encrypted.from64(txt), iv: iv));
+    } catch (ex) {
+      print('Exception: ${ex.toString()}');
+      result = ResultData(false);
+    }
+    return result;
   }
 }
