9a56b13c643a4951e399f86fc6f81c4a6b16e724
tglima <thg.limadesousa@gmail.com>
2021-09-20 22:11:09 -0300

    Ajustes no arquivo my_application.cc

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -40,15 +40,15 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar *header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "myfluttercrypto");
+    gtk_header_bar_set_title(header_bar, "MyFlutterCrypto");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   }
   else {
-    gtk_window_set_title(window, "myfluttercrypto");
+    gtk_window_set_title(window, "MyFlutterCrypto");
   }
 
-  gtk_window_set_default_size(window, 1280, 720);
+  gtk_window_set_default_size(window, 800, 450);
   gtk_widget_show(GTK_WIDGET(window));
 
   g_autoptr(FlDartProject) project = fl_dart_project_new();
