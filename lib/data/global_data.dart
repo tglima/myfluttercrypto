45ccc4bf19529a35d37f3e7f834fc6c3adb1e131
tglima <thg.limadesousa@gmail.com>
2021-10-03 19:45:09 -0300

    Remove o pacote package_info_plus e suas ref

--- lib/data/global_data.dart
+++ lib/data/global_data.dart
@@ -1,5 +1,4 @@
 class GlobalData {
   static String keyWord = "Sl15:2AqItCoPraJuCor";
-  static String nuVersion = '';
   static bool isMobileDevice = false;
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,25 +3,11 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter/material.dart';
 import 'package:myfluttercrypto/screens/home_screen.dart';
 import 'package:myfluttercrypto/utils/constant_util.dart';
-import 'package:package_info_plus/package_info_plus.dart';
 import 'data/global_data.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() async {
   GlobalData.isMobileDevice = Platform.isAndroid;
-
-  Future<void> _initPackageInfo() async {
-    PackageInfo packageInfo = PackageInfo(
-      appName: '',
-      packageName: '',
-      version: '',
-      buildNumber: '',
-      buildSignature: '',
-    );
-    final info = await PackageInfo.fromPlatform();
-    packageInfo = info;
-    GlobalData.nuVersion = packageInfo.version;
-  }
 
   runApp(const MaterialApp(
     supportedLocales: [
@@ -38,6 +24,4 @@ void main() async {
     home: HomeScreen(),
     title: ConstantUtil.nmApplication,
   ));
-
-  await _initPackageInfo();
 }
