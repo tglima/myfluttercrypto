244dca85fbd089ebac1f86c7fcb9b558eac308a2
tglima <thg.limadesousa@gmail.com>
2021-10-04 16:52:35 -0300

    Adiciona o método _setMinWindowSize

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,16 @@ import 'package:myfluttercrypto/screens/home_screen.dart';
 import 'package:myfluttercrypto/utils/constant_util.dart';
 import 'data/global_data.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:desktop_window/desktop_window.dart';
 
 void main() async {
   GlobalData.isMobileDevice = Platform.isAndroid;
+
+  Future _setMinWindowSize() async {
+    if (!GlobalData.isMobileDevice) {
+      await DesktopWindow.setMinWindowSize(ConstantUtil.minWindowSize);
+    }
+  }
 
   runApp(const MaterialApp(
     supportedLocales: [
@@ -24,4 +31,6 @@ void main() async {
     home: HomeScreen(),
     title: ConstantUtil.nmApplication,
   ));
+
+  await _setMinWindowSize();
 }
